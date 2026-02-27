b53bede661ccbdbed8b7c900459086723ae0e6e0
SamuelPerezC <samuelperezcontreras@gmail.com>
2026-02-27 09:44:17 -0500

    Add files via upload

--- EJERCICIO CALCULADORA/numero.py
+++ EJERCICIO CALCULADORA/numero.py
@@ -1,6 +1,7 @@
 class Numero:
     def __init__(self,numero):
-        self.numero = numero
+        self.numero = int(numero)
+        """CONVERTIMOS EL NUMERO STRISNG  A NUMERO ENTERO YA QUE SIN EL INT PYTHON LOS VE COMO TEXTO Y LOS CONCATENA"""
         
     def get_numero(self):
         return self.numero
